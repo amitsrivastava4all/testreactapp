460cca96a17134d6559cd99475892465ec9d7f59
Amit Srivastava <amit.srivastava4all@gmail.com>
2020-04-05 08:27:47 +0530

    Code

---
+++ precache-manifest.6b03a56690165a33d313293a094ea988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d1e4855a08da728899e1df386ad284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4fb2dbbfcc8fbac347f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "addbb5558535a928ea6f",
+    "url": "/static/js/2.11e9c32a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.11e9c32a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fb2dbbfcc8fbac347f",
+    "url": "/static/js/main.fb78c5ae.chunk.js"
+  },
+  {
+    "revision": "c50c75e4a84309e0a0d5",
+    "url": "/static/js/runtime-main.071dcc11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
